a701769743552714b2c16f188cc0b9750c76489f
Camel Ninla <rfvbkm0220@gmail.com>
2022-11-28 00:02:37 +0300

    feat: stupid block

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/CAMELNINGA/sutpid_block
+
+go 1.19
---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	// create a new blockchain instance with a mining difficulty of 2
+	blockchain := CreateBlockchain(2)
+
+	// record transactions on the blockchain for Alice, Bob, and John
+	blockchain.addBlock("Alice", "Bob", 5)
+	blockchain.addBlock("John", "Bob", 2)
+	blockchain.addBlock("John", "Bob", 10)
+
+	// check if the blockchain is valid; expecting true
+	fmt.Println(blockchain.isValid())
+}
+func CreateBlockchain(difficulty int) Blockchain {
+	genesisBlock := Block{
+		hash:      "0",
+		timestamp: time.Now(),
+	}
+	return Blockchain{
+		genesisBlock,
+		[]Block{genesisBlock},
+		difficulty,
+	}
+}
